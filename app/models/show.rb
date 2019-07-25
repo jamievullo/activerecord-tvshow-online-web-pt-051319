ea658ce8fc0dc87990aa61dddb70fd06b5528ba8
Jamie Vullo <jvullo@protonmail.com>
2019-07-25 15:26:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,12 +4,12 @@
 class Show < ActiveRecord::Base
   
   def self.highest_rating
-    binding.pry
+    #binding.pry
     self.maximum(:rating)
   end
   
   def self.most_popular_show
-    
+    binding.pry
   end
   
   def self.lowest_rating

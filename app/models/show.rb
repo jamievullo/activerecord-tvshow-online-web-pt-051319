8b151cbaae62c9236d0fbfd623177ff48c12a6e5
Jamie Vullo <jvullo@protonmail.com>
2019-07-25 15:07:09 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,34 @@
+# In this file, you will define a Show class 
+# that inherits from ActiveRecord::Base.
+
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    
+  end
+  
+  def self.most_popular_show
+    
+  end
+  
+  def self.lowest_rating
+    
+  end
+  
+  def self.least_popular_show
+    
+  end
+  
+  def self.ratings_sum
+    
+  end
+  
+  def self.popular_shows
+    
+  end
+  
+  def self.shows_by_alphabetical_order
+    
+  end
+  
+end
